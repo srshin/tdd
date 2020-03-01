b8251ef708b314081a814599eb35e0d6fd2cb84b
신상림 <sangrimshin@SrshinMac.local>
2020-03-02 00:49:20 +0900

    new tes

---
+++ tddTest/src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    int a;
+    int b;
+    Calculator() {}
+    Calculator(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+    int add () {
+        return a+b;
+    }
+}
---
+++ tddTest/src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void addTest() {
+        Calculator c = new Calculator(1,3);
+        assertEquals(4, c.add());
+    }
+
+}
